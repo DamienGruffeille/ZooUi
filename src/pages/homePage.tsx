bd54f6c4f1e26a6ecaeeabe74b2d7af3d4a77893
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-12-06 11:39:04 +0100

    Création enclosureBlock

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from "react";
+import EnclosureBlock from "../components/EnclosureBlock";
 import Header from "../components/Header";
 
+export interface Employee {
+    _id: string;
+    name: string;
+    firstName: string;
+    email: string;
+    password: string;
+    role: string;
+    zone: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 const HomePage = () => {
+    /** Récupération des zones de l'employé */
+    const [employee, setEmployee] = useState<Employee>();
+    const employeeLocalStorage = localStorage.getItem("employee");
+
+    useEffect(() => {
+        if (employeeLocalStorage) {
+            setEmployee(JSON.parse(employeeLocalStorage));
+        }
+    }, [employeeLocalStorage]);
+
+    // get enclosures from zone
+
+    // get specy by enclosure
     return (
         <>
             <Header />
 
-            <div className="id">
-                <p>
-                    Nom Prénom <br />
-                    Rôle : <br />
-                    Zone : <br />
-                </p>
-            </div>
             <main>
-                <h1>Page principale</h1>
+                {employee === null || employee === undefined ? (
+                    <ul></ul>
+                ) : (
+                    <ul>
+                        <li className="specieBlock" key={employee?._id}>
+                            <EnclosureBlock zone={employee?.zone} />
+                        </li>
+                    </ul>
+                )}
             </main>
         </>
     );
