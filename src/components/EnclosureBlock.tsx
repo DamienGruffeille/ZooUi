803704902a14644627530e77d985fb1b7a2ccc03
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-12-15 11:31:48 +0100

    OK component SpecieMovementBlock

--- src/components/EnclosureBlock.tsx
+++ src/components/EnclosureBlock.tsx
@@ -2,16 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useState, ReactElement, useEffect } from "react";
 import AsyncSelect from "react-select/async";
-import { fetchAnimalsBySpecy } from "../fetchers/animals";
-import {
-    feedSpecie,
-    putSpecieInside,
-    putSpecieOutside,
-    stimulateSpecie
-} from "../fetchers/postEvent";
+import { feedSpecie, stimulateSpecie } from "../fetchers/postEvent";
 import { getLastEvent } from "../fetchers/getEvents";
 import { createAxiosConfig } from "../functions/createAxiosConfig";
 import Specie from "../interfaces/specie";
+import SpecieMovementBlock from "./SpecieMovementBlock";
 
 type EnclosureBlockProps = {
     zone: string;
@@ -29,31 +24,19 @@ const EnclosureBlock = ({ zone }: EnclosureBlockProps): ReactElement => {
     console.log("Render");
 
     const [selectedOption, setSelectedOption] = useState<string>("");
-    const [notMovingAnimals, setnotMovingAnimals] = useState<string[]>([]);
-    const [didClickMovementButton, setdidClickMovementButton] = useState(false);
     const [didClickFeedButton, setdidClickFeedButton] = useState(false);
     const [dernierNourrissage, setdernierNourrissage] =
         useState("Pas encore nourris");
     const [derniereStimulation, setderniereStimulation] = useState(
         "Pas encore stimulés"
     );
-    const [checkBoxChecked, setCheckBoxChecked] = useState<HTMLInputElement[]>(
-        []
-    );
 
     /** Si l'employé change d'espèce, le tableau des animaux qui ne
      * bougent pas est vidé
      */
-    useEffect(() => {
-        setnotMovingAnimals([]);
-    }, [selectedOption]);
-
-    /** Décoche les checkbox à lorsqu'un bouton entrer/sortir est activé */
-    useEffect(() => {
-        if (didClickMovementButton) {
-            checkBoxChecked.forEach((checkBox) => (checkBox.checked = false));
-        }
-    }, [didClickMovementButton, checkBoxChecked]);
+    // useEffect(() => {
+    //     setnotMovingAnimals([]);
+    // }, [selectedOption]);
 
     /** fetch les espèces dans la zone de l'employé
      * si l'employé est autorisé sur toutes les zones, fetch
@@ -104,12 +87,6 @@ const EnclosureBlock = ({ zone }: EnclosureBlockProps): ReactElement => {
         return specie._id === selectedOption;
     });
 
-    const { data: animals } = useQuery({
-        queryKey: ["Animals", specie],
-        queryFn: () => fetchAnimalsBySpecy(specie?._id),
-        enabled: !!specie
-    });
-
     const { data: nourrissage, refetch: refetchNourrissage } = useQuery({
         queryKey: ["FeedEvents", specie],
         queryFn: () => getLastEvent(specie?._id, "Nourrissage"),
@@ -150,57 +127,6 @@ const EnclosureBlock = ({ zone }: EnclosureBlockProps): ReactElement => {
         }
     }, [stimulation]);
 
-    /** Gestion du tableau des animaux qu'il ne faut pas mouvementer */
-    const handleAnimalArray = (e: any) => {
-        console.log("e.target : " + e.target.id);
-        console.log(e.target.checked);
-        if (e.target.checked) {
-            if (!notMovingAnimals.includes(e.target.value)) {
-                setnotMovingAnimals((prev) => [...prev, e.target.value]);
-                console.log("added to array");
-            }
-            if (!checkBoxChecked.includes(e.target)) {
-                setCheckBoxChecked((prev) => [...prev, e.target]);
-            }
-        } else {
-            setnotMovingAnimals((prev) =>
-                prev.filter((animal) => animal !== e.target.value)
-            );
-            setCheckBoxChecked((prev) =>
-                prev.filter((checkBox) => checkBox !== e.target)
-            );
-        }
-    };
-
-    const handleSubmit = (e: any) => {
-        if (e.target.value === "Sortie") {
-            if (specie) {
-                console.log("Espèce " + specie._id);
-                console.log("eventType " + e.target.value);
-                console.log("Tab animaux pas bougé " + notMovingAnimals);
-
-                putSpecieOutside(specie._id, notMovingAnimals);
-
-                console.log("postEvent a été appelé");
-            } else {
-                console.log("postEvent n'a pas pu être appelé");
-            }
-        } else {
-            if (specie) {
-                console.log("Espèce " + specie._id);
-                console.log("eventType " + e.target.value);
-                console.log("Tab animaux pas bougé " + notMovingAnimals);
-
-                putSpecieInside(specie._id, notMovingAnimals);
-
-                console.log("postEvent a été appelé");
-            } else {
-                console.log("postEvent n'a pas pu être appelé");
-            }
-        }
-        setdidClickMovementButton(true);
-    };
-
     const handleFeeding = () => {
         if (specie) {
             feedSpecie(specie._id);
@@ -236,60 +162,7 @@ const EnclosureBlock = ({ zone }: EnclosureBlockProps): ReactElement => {
                             <label htmlFor="">{specie.name}</label>
                         </div>
 
-                        <div
-                            key={"position"}
-                            className="enclosureBlock__container__specie"
-                        >
-                            <h4>Modifier position de l'espèce :</h4>
-                            <br />
-                            <span>
-                                Sélectionnez les animaux n'ayant pas bougé :
-                            </span>
-                            <ul>
-                                {animals?.map((animal, index) => {
-                                    return (
-                                        <li key={index}>
-                                            <input
-                                                title="animal"
-                                                type="checkbox"
-                                                name={animal.name}
-                                                value={animal._id}
-                                                id={`custom-checkbox-${index}`}
-                                                // checked={checkedAnimals[index]}
-                                                onChange={(e) =>
-                                                    handleAnimalArray(e)
-                                                }
-                                            />
-                                            <label
-                                                htmlFor={`custom-checkbox-${index}`}
-                                            >
-                                                {animal.name}
-                                            </label>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                            <br />
-                            <div
-                                key={"buttons"}
-                                className="enclosureBlock__container__specie__btn"
-                            >
-                                <button
-                                    onClick={handleSubmit}
-                                    title="rentrer"
-                                    value={"Entrée"}
-                                >
-                                    Rentrer
-                                </button>
-                                <button
-                                    onClick={handleSubmit}
-                                    title="sortir"
-                                    value={"Sortie"}
-                                >
-                                    Sortir
-                                </button>
-                            </div>
-                        </div>
+                        <SpecieMovementBlock specie={specie} />
                         <div key={"autresActions"}>
                             <div
                                 key={"nourrir"}
--- src/components/SpecieMovementBlock.tsx
+++ src/components/SpecieMovementBlock.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAnimalsBySpecy } from "../fetchers/animals";
+import Specie from "../interfaces/specie";
+import { putSpecieInside, putSpecieOutside } from "../fetchers/postEvent";
 
-const SpecieMovementBlock = () => {
-    return <div>SpecieMovementBlock</div>;
+type Props = {
+    specie: Specie;
+};
+
+const SpecieMovementBlock = ({ specie }: Props) => {
+    const [notMovingAnimals, setnotMovingAnimals] = useState<string[]>([]);
+    const [checkBoxChecked, setCheckBoxChecked] = useState<HTMLInputElement[]>(
+        []
+    );
+
+    const { data: animals } = useQuery({
+        queryKey: ["Animals", specie],
+        queryFn: () => fetchAnimalsBySpecy(specie?._id),
+        enabled: !!specie
+    });
+
+    const handleAnimalArray = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            setnotMovingAnimals((prev) => [...prev, e.target.value]);
+            setCheckBoxChecked((prev) => [...prev, e.target]);
+        } else {
+            setnotMovingAnimals((prev) =>
+                prev.filter((animal) => animal !== e.target.value)
+            );
+            setCheckBoxChecked((prev) =>
+                prev.filter((checkBox) => checkBox !== e.target)
+            );
+        }
+    };
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(notMovingAnimals);
+        console.log(checkBoxChecked);
+        console.log("Bouton déclenché");
+        const mouvement = e.target as HTMLButtonElement;
+        if (mouvement.value === "Sortie") {
+            putSpecieOutside(specie._id, notMovingAnimals);
+        } else {
+            putSpecieInside(specie._id, notMovingAnimals);
+        }
+
+        checkBoxChecked.forEach((checkbox) => (checkbox.checked = false));
+        setCheckBoxChecked([]);
+        setnotMovingAnimals([]);
+    };
+
+    return (
+        <div key={"position"} className="enclosureBlock__container__specie">
+            <h4>Modifier position de l'espèce :</h4>
+            <br />
+            <span>Sélectionnez les animaux n'ayant pas bougé :</span>
+            <ul>
+                {animals?.map((animal, index) => {
+                    return (
+                        <li key={index}>
+                            <input
+                                title="animal"
+                                type="checkbox"
+                                name={animal.name}
+                                value={animal._id}
+                                id={`custom-checkbox-${index}`}
+                                // checked={checkedAnimals[index]}
+                                onChange={(e) => handleAnimalArray(e)}
+                            />
+                            <label htmlFor={`custom-checkbox-${index}`}>
+                                {animal.name}
+                            </label>
+                        </li>
+                    );
+                })}
+            </ul>
+            <br />
+            <div
+                key={"buttons"}
+                className="enclosureBlock__container__specie__btn"
+            >
+                <button onClick={handleSubmit} title="rentrer" value={"Entrée"}>
+                    Rentrer
+                </button>
+                <button onClick={handleSubmit} title="sortir" value={"Sortie"}>
+                    Sortir
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default SpecieMovementBlock;
