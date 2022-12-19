19314d5e011163f2ca60d09e674bc6f27fe9b2bc
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-12-19 16:54:16 +0100

    Début page évènements

--- src/components/EnclosureBlock.tsx
+++ src/components/EnclosureBlock.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, ReactElement, useEffect } from "react";
+import { useState, ReactElement } from "react";
 import AsyncSelect from "react-select/async";
 import { createAxiosConfig } from "../functions/createAxiosConfig";
 import Specie from "../interfaces/specie";
--- src/pages/evenementsPage.tsx
+++ src/pages/evenementsPage.tsx
@@ -1,13 +1,69 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AsyncSelect from "react-select/async";
 import Header from "../components/Header";
+import { createAxiosConfig } from "../functions/createAxiosConfig";
+import Employee from "../interfaces/employee";
+import Zone from "../interfaces/zone";
 
-const evenementsPage = () => {
+type Options = {
+    label: string;
+    value: string;
+};
+
+const config = createAxiosConfig();
+
+const EvenementsPage = () => {
+    /** Récupération des zones de l'employé */
+    const [employee, setEmployee] = useState<Employee>();
+    const employeeLocalStorage = localStorage.getItem("employee");
+
+    useEffect(() => {
+        if (employeeLocalStorage) {
+            setEmployee(JSON.parse(employeeLocalStorage));
+        }
+    }, [employeeLocalStorage]);
+
+    // get all zones of the employee
+    const loadZones = (
+        inputValue: string,
+        callback: (options: Options[]) => void
+    ) => {
+        axios
+            .get("http://localhost:3000/api/zones/get", config)
+            .then((response) => {
+                const options: Options[] = [];
+                response.data.zones.forEach((zone: Zone) => {
+                    options.push({ label: zone.name, value: zone._id });
+                });
+                callback(options);
+            });
+    };
+
+    // get all enclosures of each zone
+
     return (
         <>
             <Header />
-            <div>evenementsPage</div>
+            <main>
+                <h3>Evènements</h3>
+                <div>
+                    <h4>Liste des évènements</h4>
+                    <div>
+                        <span>Par zone : </span>
+                        <AsyncSelect
+                            cacheOptions
+                            loadOptions={loadZones}
+                            defaultOptions
+                            placeholder="Sélectionner la zone"
+                            // onChange={onChangeSelectedOption}
+                        />
+                    </div>
+                </div>
+                <form action="">Ajouter un évènement</form>
+            </main>
         </>
     );
 };
 
-export default evenementsPage;
+export default EvenementsPage;
